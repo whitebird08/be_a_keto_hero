735ccf36291080bb56c75a2a31e04acf296a874e
Ryan Bird <ryan01bird@gmail.com>
2015-09-19 19:52:48 -0600

    added db&collections and posts for new food&meal

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('monk')('localhost/keto');
+var foodsCollection = db.get('foods');
+var mealsCollection = db.get('meals');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ketoHero' });
@@ -13,20 +17,22 @@ router.get('/food/newfood', function(req, res, next){
 });
 
 router.get('/food', function(req, res, next){
-  res.render('food')
-
+  foodsCollection.find({}, function (err, foods) {
+  res.render('food'), {allFoods: foods}
+  });
 });
 
+
+
 router.get('/food/:id', function(req, res, next) {
-  albumCollection.findOne({_id: req.params.id}, function (err, record) {
-    res.render('albums/show'); // ,{allFoods: foods} <-object needs to be inserted in parens
-  });
+  // foodsCollection.findOne({_id: req.params.id}, function (err, food) {
+    res.render('food/:id/show'); // ,{allFoods: foods} <-object needs to be inserted in parens
+  // });
 });
 //
 
 router.get('/food/:id/edit', function(req, res, next) {
-  // albumCollection.findOne({_id: req.params.id}, function (err, record) {
-  //    console.log(record)
+  // foodsCollection.findOne({_id: req.params.id}, function (err, record) {
     res.render('food/edit');// ,{theFood: food} <-object needs to be inserted in parens 
 
 });
@@ -44,17 +50,37 @@ router.get('/meal', function(req, res, next){
 });
 
 router.get('/meal/:id', function(req, res, next) {
-  albumCollection.findOne({_id: req.params.id}, function (err, record) {
-    res.render('albums/show'); // ,{allMeals: meals} <-object needs to be inserted in parens
+  mealsCollection.findOne({_id: req.params.id}, function (err, record) {
+    res.render('meal/show'); // ,{allMeals: meals} <-object needs to be inserted in parens
   });
 });
 
 
 router.get('/meal/:id/edit', function(req, res, next) {
-  // albumCollection.findOne({_id: req.params.id}, function (err, record) {
-  //    console.log(record)
+  // mealsCollection.findOne({_id: req.params.id}, function (err, record) {
     res.render('meal/edit');// ,{theMeal: meal} <-object needs to be inserted
 
+});
+
+router.post('/food', function(req, res, next) {
+  foodsCollection.insert({ total_grams: req.body.total_grams, 
+                           food_name: req.body.food_name, 
+                           protien_grams: req.body.protien_grams, 
+                           fat_grams: req.body.fat_grams,
+                           carbs_grams: req.body.carbs_grams,
+  });
+  res.redirect('/food');
+});
+
+router.post('/meal', function(req, res, next) {
+  mealsCollection.insert({ meal_name: req.body.meal_name, 
+                           meal_item1: req.body.meal_item, 
+                           meal_item2: req.body.meal_item,
+                           meal_item3: req.body.meal_item,
+                           meal_item4: req.body.meal_item,
+                           meal_item5: req.body.meal_item  
+  });
+  res.redirect('/meal');
 });
 
 
